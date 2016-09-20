c0e99d920bb4cb127ce81c514a20a7cbcb13ddb7
ufukbal <balufuk91@gmail.com>
2016-09-20 19:12:06 +0300

    Initial Commit

---
+++ Assets/MapDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDisplay : MonoBehaviour {
+
+	public Renderer textureRenderer;
+
+	public void DrawTexture(Texture2D texture){
+
+
+
+
+		textureRenderer.sharedMaterial.mainTexture = texture; // using shared material for edit mode display
+		textureRenderer.transform.localScale = new Vector3 (texture.width, 1, texture.height);
+
+	}
+
+}
